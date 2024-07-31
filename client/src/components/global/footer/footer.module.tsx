82d52575b8847f160d9b1e3d61dcf8b35ec401fd
hainnotlazy <hain6621.dev@gmail.com>
2024-07-31 21:01:13 +0700

    feat(client): footer

---
+++ client/src/components/global/footer/footer.module.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+	return (
+		<footer className="py-2.5 bg-red-500 text-white">
+			<div className="container flex items-center justify-center px-2 mx-auto">
+				<p className="text-sm font-semibold">© 2024 ShopLOL.fake - Made by Hòa & Kiệt with 💚</p>
+			</div>
+		</footer>
+	);
+}
