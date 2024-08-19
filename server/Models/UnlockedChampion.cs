4c457297d42436d7b62efbc54d2b82a4be807e81
Darkieeee <vankiet742@gmail.com>
2024-08-19 20:07:53 +0700

    chore(backend): add primary key

--- server/Models/UnlockedChampion.cs
+++ server/Models/UnlockedChampion.cs
@@ -1,5 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 namespace server.Models;
 
+[Table("UnlockedChampions")]
+[PrimaryKey(nameof(AccountId), nameof(ChampionId))]
 public class UnlockedChampion {
 	public required string AccountId;
 	public required int ChampionId;
