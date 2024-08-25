f26a36d309d3a847da4e59424dd8ae99dd734302
Darkieeee <vankiet742@gmail.com>
2024-08-25 19:03:12 +0700

    feat(frontend): add admin sidebar

--- client/src/layouts/admin/default/layout.module.tsx
+++ client/src/layouts/admin/default/layout.module.tsx
@@ -1,10 +1,19 @@
-import { AdminSidebar } from "@/components/common/sidebar/admin-sidebar.module";
+import { AdminSidebar, AdminSidebarItem } from "@/components/common/sidebar/admin-sidebar.module";
+import { MdManageAccounts, MdOutlineDashboard, MdVideogameAsset } from "react-icons/md";
+import { FaUserFriends } from "react-icons/fa";
+import { GrTransaction } from "react-icons/gr";
 
 export function AdminDefaultLayout() {
 	return (
 		<main>
-			<div>
-				<AdminSidebar />
+			<div className="flex">
+				<AdminSidebar>
+					<AdminSidebarItem icon={<MdOutlineDashboard/>} text="Dashboard" active/>
+					<AdminSidebarItem icon={<MdManageAccounts/>} text="Accounts"/>
+					<AdminSidebarItem icon={<GrTransaction/>} text="Transaction Histories"/>
+					<AdminSidebarItem icon={<MdVideogameAsset/>} text="Game Assets"/>
+					<AdminSidebarItem icon={<FaUserFriends/>} text="Users"/>
+				</AdminSidebar>
 			</div>
 			<div className="content"></div>
 		</main>
