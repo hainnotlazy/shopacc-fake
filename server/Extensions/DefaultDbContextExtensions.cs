3af31e9e7cb70f7b1433036e4bddb8f9c4d845aa
hainnotlazy <hain6621.dev@gmail.com>
2024-08-28 04:27:00 +0100

    enhance(backend): seed db

--- server/Extensions/DefaultDbContextExtensions.cs
+++ server/Extensions/DefaultDbContextExtensions.cs
@@ -1,25 +1,39 @@
 using Microsoft.EntityFrameworkCore;
 using server.Models;
 
-namespace server.Extensions {
-	public static class DefaultDbContextExtension {
-		public static void SeedData(this ModelBuilder modelBuilder) {
-			IConfigurationRoot configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json")
-																																	 .AddEnvironmentVariables()
-																																	 .Build();
+namespace server.Extensions
+{
+	public static class DefaultDbContextExtension
+	{
+		public static void SeedData(this ModelBuilder modelBuilder)
+		{
+#pragma warning disable CS8600
+			string runningMode = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+			string envFileName =
+				runningMode == "Development" ? "appsettings.Development.json" : "appsettings.json";
 
-			modelBuilder.Entity<User>().HasData(
-				new User {
-					Id = 1,
-					Email = "shopacc.fake@gmail.com",
-					Password = BCrypt.Net.BCrypt.HashPassword(configuration.GetValue<string>("AppSettings:AppKey")),
-					Username = "admin",
-					IsAdmin = true,
-					UseDarkMode = false,
-					EmailVerificationCode = 0,
-					IsEmailVerified = true
-				}
-			);
+			IConfigurationRoot configuration = new ConfigurationBuilder()
+				.AddJsonFile(envFileName)
+				.AddEnvironmentVariables()
+				.Build();
+
+			modelBuilder
+				.Entity<User>()
+				.HasData(
+					new User
+					{
+						Id = 1,
+						Email = "shopacc.fake@gmail.com",
+						Password = BCrypt.Net.BCrypt.HashPassword(
+							configuration.GetValue<string>("AppSettings:AppKey")
+						),
+						Username = "admin",
+						IsAdmin = true,
+						UseDarkMode = false,
+						EmailVerificationCode = 0,
+						IsEmailVerified = true
+					}
+				);
 		}
 	}
 }
